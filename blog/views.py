8e20bb8e1f4df1e8c1503359f17315fb15efe3bc
Njsc <18351993266@163.com>
2016-11-04 16:47:14 +0800

    add delete post and drafts post publish func

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -42,3 +42,20 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def show_post_drafts(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('create_date')
+    return render(request, 'blog/post_drafts_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=post.pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
